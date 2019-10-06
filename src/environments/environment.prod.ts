7736916b3fa45dd27f1badc6eecf74ef6617540b
Bruno Batista <brunorafael.o@hotmail.com>
2019-10-06 14:30:50 -0300

    Alteração de arquivos docker e outros ajustes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tendtudo-business-api.herokuapp.com',
+  apiUrl: 'http://172.28.0.4:8080',
   tokenWhitelistedDomains: [ 'tendtudo-business-api.herokuapp.com' ],
  //tokenWhitelistedDomains: [ 'localhost:8080' ],
  tokenBlacklistedRoutes: [ '\/oauth\/token'  ]
