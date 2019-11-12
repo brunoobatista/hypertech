208b015b8475352d213b8afd38ab6d8868116c04
Bruno Batista <brunorafael.o@hotmail.com>
2019-11-12 20:31:54 -0300

    Correções de commit, inicio da implementação do gráfico

--- src/app/vendas/input-produto/input-produto.component.ts
+++ src/app/vendas/input-produto/input-produto.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Input, ContentChild, TemplateRef, DoCheck } from '@angular/core';
-import { Produto } from 'src/app/model/Produto';
-import { NgForOfContext } from '@angular/common';
+import { Component, OnInit, Input, HostListener, Output, EventEmitter, ViewChildren, QueryList, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { ChildActivationStart } from '@angular/router';
 
 @Component({
   selector: 'app-input-produto',
@@ -9,17 +8,75 @@ import { NgForOfContext } from '@angular/common';
 })
 export class InputProdutoComponent implements OnInit {
 
-  @Input() produtos: Produto[];
+  pclass = 'product-selected';
 
-  @ContentChild(TemplateRef) produtoTemplate: TemplateRef<NgForOfContext<Produto>>;
+  @Input() produtosInput = [];
+  @Input() placeholder: string;
+  @Input() label: string;
 
-  constructor() {
+  @Output() changeInput = new EventEmitter();
+  @Output() closeModal = new EventEmitter();
+  @Output() addProducts = new EventEmitter();
 
+  @ViewChildren('productsList', { read: ElementRef }) productsSelected: QueryList<any>;
+
+  @ViewChild('divLiveSearch', {read: ElementRef}) divLiveSearch: any;
+  @ViewChild('inputName', {read: ElementRef}) inputName: any;
+
+  constructor(
+    private render2: Renderer2,
+  ) {
   }
 
   ngOnInit() {
+    
   }
 
-  
+  @HostListener("keyup", ["$event"])
+    handleInput(event: any) {
+        if (event.keyCode === 27) {
+          this.close();
+          return;
+        }
+    
+        if (event.keyCode === 8 && this.inputName.nativeElement.value.length < 3) {
+          this.removeChildren(this.divLiveSearch.nativeElement);
+        }
+        this.changeInput.emit(event.target.value);
+    } 
+
+  productSelected(event) {
+    let parent = event.target.parentElement;
+    parent.classList.toggle(this.pclass);
+  }
+
+  close() {
+    this.closeModal.emit();
+    this.removeChildren(this.divLiveSearch.nativeElement);
+  }
+
+  add() {
+    const produtosEl = [];
+    this.productsSelected.map(element => {
+      if (element.nativeElement.classList.contains(this.pclass)) {
+        produtosEl.push(parseInt(element.nativeElement.id));
+      }
+    });
+
+    const produtosList = this.produtosInput.filter(p => {
+      if (produtosEl.indexOf(p.id) > -1) {
+        return p;
+      }
+    });
+    this.addProducts.emit(produtosList);
+  }
+
+  removeChildren(div: any) {
+    Array.from(div.children).forEach(c => {
+      this.render2.removeChild(div, c);
+    });
+    this.produtosInput = [];
+    this.inputName.nativeElement.value = '';
+  }
 
 }
